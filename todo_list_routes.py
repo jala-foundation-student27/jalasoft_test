2013333c7fa4370def0e6c5aa0d471099422e3c4
jala-foundation-student27 <112252142+jala-foundation-student27@users.noreply.github.com>
2023-11-18 14:06:38 -0300

    CRUD routes for todo_list_model and error management

---
+++ todo_list_routes.py
@@ -0,0 +1,90 @@
+from flask import Blueprint,jsonify,request,abort
+from todo_list_model import Todo_list_model,Status
+from db_connection import get_todo_collection,get_db
+from bson import ObjectId
+import datetime
+
+todo_bp = Blueprint("todo_bp",__name__)
+
+db=get_db()
+soft_deleted_todo_list = db.soft_deleted_todo_list
+todo_collection = get_todo_collection()
+
+@todo_bp.route('/todo_list/<id>',methods=["GET"])
+def get(id):
+    try:
+        response = todo_collection.find_one({"_id":ObjectId(id)})
+        response.update({"_id":id})
+    except:
+        abort(404)
+    return jsonify(response)
+
+@todo_bp.route('/todo_list',methods=["POST"])
+def post():
+
+    try:
+        request_json = request.get_json()
+        now = datetime.datetime.now()
+        parsed_data = {"task":request_json["task"],
+                    "deadline":request_json["deadline"],
+                        "category":request_json["category"],
+                        "status":Status(request_json["status"]),
+                        "description":request_json["description"],
+                        "lastUpdated":now
+                        }
+        #Todo_list_model(**parsed_data)  # validate if the request match the todo_list model
+        insert_response = todo_collection.insert_one(request_json) # Object id cant be casted to json
+    except:
+        abort(400)
+    request_json.update({"_id":str(request_json["_id"]),"lastUpdated":now})
+    return jsonify(request_json),201,{"location":f"/todo_list/{insert_response.inserted_id}"}
+
+@todo_bp.route('/todo_list/<id>',methods=["PUT"])
+def put(id):
+    try:
+        request_json = request.get_json()
+        now = datetime.datetime.now()
+        parsed_data = {"task":request_json["task"],
+                    "deadline":request_json["deadline"],
+                        "category":request_json["category"],
+                        "status":Status(request_json["status"]),
+                        "description":request_json["description"],
+                        "lastUpdated":now}
+        Todo_list_model(**parsed_data)  # validate if the request match the todo_list model
+        insert_response = todo_collection.replace_one({"_id":ObjectId(id)},request_json) # Object id cant be casted to json
+    except:
+        abort(400)
+    request_json.update({"_id":id,"lastUpdated":now})
+    return jsonify(request_json),201
+
+
+@todo_bp.route('/todo_list/<id>',methods=["DELETE"]) # deleted files will go to soft deleted documents and removed from todo_colection
+def delete(id): 
+    try:
+        response = todo_collection.find_one({"_id":ObjectId(id)})
+    except:
+        abort(404)
+    try:
+        soft_deleted_todo_list.insert_one(response)
+        todo_collection.delete_one({"_id":ObjectId(id)})
+    except:
+        abort(400)
+    return "",204
+
+@todo_bp.route('/todo_list/deleted/<id>',methods=["GET"])
+def get_soft_deleted(id):
+    try:
+        response = soft_deleted_todo_list.find_one({"_id":ObjectId(id)})
+        response.update({"_id":id})
+    except:
+        abort(404)
+    return jsonify(response)
+
+
+@todo_bp.errorhandler(400)
+def invalid_request(e):
+    return jsonify({"error":"invalid request"}),400
+
+@todo_bp.errorhandler(404)
+def invalid_request(e):
+    return jsonify({"error":"Object Not Found"}),404
