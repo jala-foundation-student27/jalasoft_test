6cb0a705f2b59c01c3acb2c0101460a63586cdcb
jala-foundation-student27 <112252142+jala-foundation-student27@users.noreply.github.com>
2023-11-18 14:10:40 -0300

    Main aplication with the the register of routes from todo_list_routes.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from todo_list_routes import todo_bp
+from flask import Flask
+
+app = Flask(__name__)
+app.register_blueprint(todo_bp) # all routes for todo are registred in app
+
+if __name__ == "__main__":
+    app.run(debug=True)
